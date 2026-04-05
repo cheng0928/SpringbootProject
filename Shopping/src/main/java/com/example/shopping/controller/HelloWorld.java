d4566dcce265d25cd140776938597ccba0835bd5
一步 <linyuchen0928@gmail.com>
2026-04-05 17:14:47 +0800

    practice day1-3

--- Shopping/src/main/java/com/example/shopping/controller/HelloWorld.java
+++ Shopping/src/main/java/com/example/shopping/controller/HelloWorld.java
@@ -1,12 +1,27 @@
 package com.example.shopping.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.shopping.entity.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorld {
-    @GetMapping("/hello")
-        public String hello(){
-        return "你好阿";
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "nickname")String name,@RequestParam(value = "telephone" ,required = true) String phone){
+        System.out.println("nickname = " + name);
+        System.out.println("telephone =" + phone);
+        return "Get請求";
+    }
+
+    @RequestMapping(value = "/hello2",method = RequestMethod.POST)
+    public String hello2(String name,String phone){
+        System.out.println("nickname = " + name);
+        System.out.println("telephone =" + phone);
+        return "Post請求";
+    }
+
+    @RequestMapping(value = "/hello3",method = RequestMethod.POST)
+    public String hello3(@RequestBody User user){
+        System.out.println(user);
+        return "Post請求";
     }
 }
---
+++ Shopping/src/main/java/com/example/shopping/entity/User.java
@@ -0,0 +1,11 @@
+package com.example.shopping.entity;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String password;
+
+}
